6c1fff439d2c82702b469d9b83666a536181cd4c
Arthur Robine <arthur.robine@epitech.eu>
2022-02-01 19:52:58 +0100

    feat(front/login) project is created and dockerized, basic routing and navbar

---
+++ front/src/pages/LoginPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function LoginPage() {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <h2>Login page</h2>
+        </header>
+      </div>
+  );
+}
+
+export default LoginPage;
---
+++ front/src/pages/RegisterPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function RegisterPage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Register page</h2>
+      </header>
+    </div>
+  );
+}
+
+export default RegisterPage;
