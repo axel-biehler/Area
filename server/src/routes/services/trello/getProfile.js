a72baa72b22b31d9828126bcab40070789d30323
Axel Biehler <axel.biehler@epitech.eu>
2022-02-06 17:22:12 +0100

    feat(trello-oauth): add link and test request

---
+++ server/src/routes/services/trello/getProfile.js
@@ -0,0 +1,46 @@
+const OAuth = require('oauth');
+const { promisify } = require('util');
+const { User } = require('../../../database');
+
+const getProfileTest = async (req, res) => {
+  const oauth = new OAuth.OAuth(
+    'https://trello.com/1/OAuthGetRequestToken',
+    'https://trello.com/1/OAuthGetAccessToken',
+    process.env.TRELLO_API_KEY,
+    process.env.TRELLO_API_SECRET,
+    '1.0A', null, 'HMAC-SHA1',
+  );
+
+  const u = await User.findOne(req.userId);
+
+  if (u == null) {
+    res.status(500).json({
+      status: false,
+      error: 'user not found',
+    });
+    return;
+  }
+
+  const getProfile = promisify(oauth.get.bind(oauth));
+
+  try {
+    let body = await getProfile('https://api.trello.com/1/members/me',
+      u.trelloToken,
+      u.trelloTokenSecret);
+
+    body = JSON.parse(body);
+
+    res.json({
+      status: true,
+      body,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      status: false,
+      error: 'internal error',
+    });
+  }
+};
+
+module.exports = getProfileTest;
---
+++ server/src/routes/services/trello/link.js
@@ -0,0 +1,59 @@
+const OAuth = require('oauth');
+const { User } = require('../../../database');
+
+const link = async (req, res) => {
+  const { oauthVerifier, oauthToken } = req.body;
+  const oauth = new OAuth.OAuth(
+    'https://trello.com/1/OAuthGetRequestToken',
+    'https://trello.com/1/OAuthGetAccessToken',
+    process.env.TRELLO_API_KEY,
+    process.env.TRELLO_API_SECRET,
+    '1.0A', null, 'HMAC-SHA1',
+  );
+
+  if (typeof oauthVerifier !== 'string' || oauthVerifier == null) {
+    res.status(400).json({
+      status: false,
+      error: 'bad format for oauth verifier',
+    });
+    return;
+  }
+  if (typeof oauthToken !== 'string' || oauthToken == null) {
+    res.status(400).json({
+      status: false,
+      error: 'bad format for oauth token',
+    });
+    return;
+  }
+
+  const u = await User.findOne(req.userId);
+
+  if (u == null) {
+    res.status(500).json({
+      status: false,
+      error: 'user not found',
+    });
+    return;
+  }
+
+  try {
+    oauth.getOAuthAccessToken(u.trelloToken, u.trelloTokenSecret, oauthVerifier,
+      (error, accessToken, accessTokenSecret) => {
+        u.trelloToken = accessToken;
+        u.trelloTokenSecret = accessTokenSecret;
+        u.save();
+      });
+
+    res.json({
+      status: true,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      status: false,
+      error: 'internal error',
+    });
+  }
+};
+
+module.exports = link;
