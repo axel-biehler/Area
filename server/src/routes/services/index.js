b6fb2127c66e3d2421f1236004cec5c1cdf704a5
Jonathan Cohen <jonathan.cohen@epitech.eu>
2022-02-10 15:29:34 +0100

    [FIX] middleware

--- server/src/routes/services/index.js
+++ server/src/routes/services/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const { authMiddleware } = require('../../authentication');
 
 const router = express.Router();
+router.use(authMiddleware);
 router.post('/twitter/connect', require('./twitter/connect'));
 router.post('/twitter/link', require('./twitter/link'));
 router.get('/twitter/settings', require('./twitter/getSettingsExample'));
 
-router.use(authMiddleware);
 router.post('/github/validate', require('./github/validate'));
 router.get('/github/user', require('./github/getUserExample'));
 
