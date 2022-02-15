424117f7c590b4a667249232a8712b8037a1d240
Alexandre Sauner <alexandre.snr@icloud.com>
2022-02-16 00:12:09 +0100

    feat(unlink): add unlink for github, trello, twitter

--- server/src/routes/services/index.js
+++ server/src/routes/services/index.js
@@ -6,12 +6,15 @@ router.use(authMiddleware);
 router.post('/twitter/connect', require('./twitter/connect'));
 router.post('/twitter/link', require('./twitter/link'));
 router.get('/twitter/getProfile', require('./twitter/getProfile'));
+router.get('/twitter/unlink', require('./twitter/unlink'));
 
 router.post('/trello/connect', require('./trello/connect'));
 router.post('/trello/link', require('./trello/link'));
 router.get('/trello/getProfile', require('./trello/getProfile'));
+router.get('/trello/unlink', require('./trello/unlink'));
 
 router.post('/github/validate', require('./github/validate'));
 router.get('/github/user', require('./github/getUserExample'));
 router.get('/github/env', require('./github/env'));
+router.get('/github/unlink', require('./github/unlink'));
 module.exports = router;
---
+++ server/src/routes/services/twitter/unlink.js
@@ -0,0 +1,31 @@
+const { User } = require('../../../database');
+
+const unlink = async (req, res) => {
+  try {
+    const user = await User.findById(req.userId);
+
+    if (user == null) {
+      return res.json({
+        status: false,
+        error: 'User not found.',
+      });
+    }
+
+    user.twitterId = undefined;
+    user.twitterAccess = undefined;
+    user.twitterRefresh = undefined;
+
+    await user.save();
+
+    return res.send({ status: true });
+  } catch (err) {
+    console.error(err);
+
+    return res.status(500).json({
+      status: false,
+      error: 'internal validate error',
+    });
+  }
+};
+
+module.exports = unlink;
