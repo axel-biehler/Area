65dc9452492b3c2627853a5df7e0ce07ab632755
Axel Biehler <axel.biehler@epitech.eu>
2022-02-15 09:35:15 +0100

    feat(instance): add create instance

--- server/src/routes/instances/createInstance.js
+++ server/src/routes/instances/createInstance.js
@@ -1,4 +1,5 @@
 const servicesAction = require('../../actions');
+const servicesReactions = require('../../reactions');
 const Instance = require('../../database/Instance');
 
 const verifyAction = (action) => {
@@ -7,7 +8,7 @@ const verifyAction = (action) => {
       || typeof action.serviceName !== 'string'
       || typeof action.displayName !== 'string'
       || typeof action.webhookId !== 'string'
-      || action.params !== undefined) { throw Error('wrong member type of action widget'); }
+      || action.params === undefined) { throw Error('wrong member type of action widget'); }
 
   const serviceMetadata = servicesAction.find((svc) => svc.name === action.serviceName);
   if (serviceMetadata === undefined) { throw Error('action invalid service'); }
@@ -16,7 +17,8 @@ const verifyAction = (action) => {
   if (widgetMetadata === undefined) { throw Error('action invalid widget'); }
 
   const invalidParams = widgetMetadata.params.some((p) => {
-    if (!(p.name in action.params) || action.params[p.name] === p.type) {
+    const params = action.params.find((par) => par.name === p.name);
+    if (params === undefined || params.type !== p.type) {
       return true;
     }
     return false;
@@ -29,28 +31,27 @@ const verifyReaction = (reaction) => {
   if (typeof reaction.name !== 'string'
       || typeof reaction.serviceName !== 'string'
       || typeof reaction.displayName !== 'string'
-      || typeof reaction.params !== 'object') { throw Error('wrong member type of reaction widget'); }
+      || reaction.params === undefined) { throw Error('wrong member type of reaction widget'); }
 
-  const serviceMetadata = servicesAction.find((svc) => svc.name === reaction.serviceName);
+  const serviceMetadata = servicesReactions.find((svc) => svc.name === reaction.serviceName);
   if (serviceMetadata === undefined) { throw Error('reaction invalid service'); }
 
   const widgetMetadata = serviceMetadata.widgets.find((wdg) => wdg.name === reaction.name);
   if (widgetMetadata === undefined) { throw Error('reaction invalid widget'); }
 
   const invalidParams = widgetMetadata.params.some((p) => {
-    if (!(p.name in reaction.params) || reaction.params[p.name] === p.type) {
+    const params = reaction.params.find((par) => par.name === p.name);
+    if (params === undefined || params.type !== p.type) {
       return true;
     }
     return false;
   });
-  if (invalidParams) { throw Error('reaction invalid parameter'); }
+  if (invalidParams) { throw Error('action invalid parameter'); }
 };
 
 const createInstance = async (req, res) => {
   const { action, reaction } = req.body;
   const { userId } = req;
-
-  console.log(action);
 
   try {
     verifyAction(action);
@@ -62,7 +63,7 @@ const createInstance = async (req, res) => {
       reaction,
     });
 
-    console.log(instance);
+    await instance.save();
   } catch (err) {
     console.error(err);
     res.status(500).json({
--- server/src/routes/instances/index.js
+++ server/src/routes/instances/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const { authMiddleware } = require('../../authentication');
 
 const router = express.Router();
+router.use(authMiddleware);
+
 router.post('/', require('./createInstance'));
 
 module.exports = router;
