092c2540af315c335afb2744cedefc275c272ea0
Alexandre Sauner <alexandre.snr@icloud.com>
2022-02-23 17:46:04 +0100

    feat(mobile/instances): add list and deletion of instances

--- mobile/src/screens/Home.js
+++ mobile/src/screens/Home.js
@@ -1,21 +1,53 @@
 import { useNavigation } from '@react-navigation/native';
 import React, { useState, useEffect } from 'react';
-import { Text, StyleSheet } from 'react-native';
-import { FAB } from 'react-native-paper';
-import { token as tokenStorage } from '../utils/storage';
+import { StyleSheet, View } from 'react-native';
+import { Card, FAB, Button } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import request from '../api/request';
+
+const Instance = ({ instance, refreshList }) => {
+  const del = async () => {
+    await request(`/instances/${instance._id}`, 'DELETE');
+    await refreshList();
+  };
+
+  return (
+    <Card style={styles.instanceView}>
+      <Card.Title title={instance.action.displayName} />
+      <Icon
+        style={styles.instanceArrow}
+        size={24}
+        name="arrow-down-circle-outline"
+      />
+      <Card.Title title={instance.reaction.displayName} />
+      <Card.Actions>
+        <Button onPress={del}>Delete</Button>
+      </Card.Actions>
+    </Card>
+  );
+};
 
 const Home = () => {
-  const [token, setToken] = useState('');
+  const [instances, setInstances] = useState([]);
   const navigation = useNavigation();
 
+  const refreshList = async () => {
+    const res = await request('/instances');
+    setInstances(res.instances);
+  };
+
   useEffect(() => {
-    (async () => {
-      setToken(await tokenStorage.get());
-    })();
-  });
+    refreshList();
+  }, []);
   return (
     <>
-      <Text>{token}</Text>
+      <View>
+        {instances.map(i => {
+          return (
+            <Instance key={i._id} instance={i} refreshList={refreshList} />
+          );
+        })}
+      </View>
       <FAB
         style={styles.fab}
         small
@@ -32,6 +64,12 @@ const styles = StyleSheet.create({
     margin: 16,
     right: 0,
     bottom: 0,
+  },
+  instanceView: {
+    margin: 4,
+  },
+  instanceArrow: {
+    marginLeft: 32,
   },
 });
 
