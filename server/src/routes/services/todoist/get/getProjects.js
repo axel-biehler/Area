17e286f7a04d071d9a53478a2c3d09f600de00b5
Maxime <maxime.frechard@epitech.eu>
2022-03-06 16:36:34 +0100

    add reaction reddit and todoist

---
+++ server/src/routes/services/todoist/get/getProjects.js
@@ -0,0 +1,42 @@
+const fetch = require('node-fetch');
+const { User } = require('../../../../database');
+
+const getProjects = async (req, res) => {
+  try {
+    const user = await User.findById(req.userId);
+    if (!user) {
+      throw Error('User not found.');
+    }
+
+    if (!user.todoistAccessToken) {
+      throw Error('User not register to todoist account.');
+    }
+
+    const request = await fetch('https://api.todoist.com/rest/v1/projects', {
+      method: 'GET',
+      body: undefined,
+      headers: { Authorization: `Bearer ${user.todoistAccessToken}` },
+    });
+    const data = await request.json();
+
+    let response = [];
+
+    data.forEach((element) => {
+      const info = { name: element.name, value: String(element.id), type: 'string' };
+      console.log(info);
+      response = response.concat(info);
+    });
+
+    console.log(response);
+    return res.json(response);
+  } catch (err) {
+    console.error(err);
+
+    return res.status(500).json({
+      status: false,
+      error: 'internal validate error',
+    });
+  }
+};
+
+module.exports = getProjects;
