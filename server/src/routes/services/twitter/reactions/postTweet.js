32eb38948c09102266ddf89fec3072d12e14fb4e
Axel Biehler <axel.biehler@epitech.eu>
2022-03-14 14:35:23 +0100

    feat(twitter): add postTweet

--- server/src/routes/services/twitter/reactions/postTweet.js
+++ server/src/routes/services/twitter/reactions/postTweet.js
@@ -1,50 +1,32 @@
 const fetch = require('node-fetch');
-const crypto = require('crypto');
-const OAuth = require('oauth-1.0a');
 const { User } = require('../../../../database');
-
-const consumerKey = process.env.TWITTER_CONSUMER_KEY;
-const consumerSecret = process.env.TWITTER_CONSUMER_SECRET;
-
-const endpointURL = 'https://api.twitter.com/2/tweets';
+const refresh = require('../refresh');
 
 const postTweet = async (instance) => {
-  const u = User.findById(instance.userId);
+  const u = await User.findById(instance.userId);
 
-  const oauth = OAuth({
-    consumer: {
-      key: consumerKey,
-      secret: consumerSecret,
-    },
-    signature_method: 'HMAC-SHA1',
-    hash_function: (baseString, key) => crypto.createHmac('sha1', key).update(baseString).digest('base64'),
-  });
+  await refresh(instance.userId);
 
-  const token = {
-    key: u.twitterAccess,
-    secret: u.twitterRefresh,
-  };
-
-  const authHeader = oauth.toHeader(oauth.authorize({
-    url: endpointURL,
-    method: 'POST',
-  }, token));
+  if (u == null) {
+    throw Error('user not found');
+  }
 
   const params = instance.reaction.params.reduce((acc, cur) => ({ ...acc, [cur.name]: cur.value }),
     {});
 
-  const req = await fetch(endpointURL, {
-    method: 'POST',
-    body: params.text,
+  const res = await fetch('https://api.twitter.com/2/tweets', {
+    method: 'POSt',
     headers: {
-      Authorization: authHeader.Authorization,
-      'user-agent': u.twitterId,
-      'content-type': 'application/json',
-      accept: 'application/json',
+      Authorization: `Bearer ${u.twitterAccess}`,
+    },
+    body: {
+      text: params.text,
     },
   });
-  const test = await req.json();
-  console.log(test);
+
+  const body = await res.json();
+
+  console.log(body);
 };
 
 module.exports = postTweet;
